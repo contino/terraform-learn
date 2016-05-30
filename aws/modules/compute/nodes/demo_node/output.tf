78ddc5e561d2f4ddfec9bdc18450254bb03c42d3
Joshua Miller <joshua.miller@fitsnips.net>
2016-05-30 00:51:21 -0700

    fixed wrong name on varible in compute demo_node output

--- aws/modules/compute/nodes/demo_node/output.tf
+++ aws/modules/compute/nodes/demo_node/output.tf
@@ -1,3 +1,3 @@
 # outputs produced at the end of a terraform apply: e.g. the id and ip of an instance
 output "demo_node_id"    { value = "${join(",", aws_instance.demo-node.*.id)}" }
-output "demo_node_sg_id" { value = "${join(",", aws_instance.demo-node.*.public_ip)}" }
+output "demo_node_ip" { value = "${join(",", aws_instance.demo-node.*.public_ip)}" }

76cae80f8fa315771fd873562e388e4809e215f0
jordzn <jordan.taylor@sendachi.com>
2016-08-13 13:43:58 +0100

    Added modular outputs

--- aws/modules/compute/nodes/demo_node/output.tf
+++ aws/modules/compute/nodes/demo_node/output.tf
@@ -1,4 +1,5 @@
 # outputs produced at the end of a terraform apply: id of the instances, id of
 # security group(s) they are part of
-output "demo_node_instance_id" { value = "${join(",", aws_instance.demo-node.*.id)}" }
-output "demo_node_sg_id"       { value = "${aws_security_group.demo-sg.id}" }
+output "demo_node_instance_id"      { value = "${join(",", aws_instance.demo-node.*.id)}" }
+output "demo_node_ip"               { value = "${join(",", aws_instance.demo-node.*.public_ip}" }
+output "demo_node_dependency_sg_id" { value = "${aws_security_group.demo-sg.id}" }

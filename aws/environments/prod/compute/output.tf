aaf32fceafa7d6dcc6e90a903a7c38b6f6090c70
jordzn <jordan.taylor@sendachi.com>
2016-08-13 13:43:39 +0100

    Added outputs

--- aws/environments/prod/compute/output.tf
+++ aws/environments/prod/compute/output.tf
@@ -1,2 +1,9 @@
 # outputs produced at the end of a terraform apply: id of the instances, id of
 # security group(s) they are part of
+output "demo_node_id"    { value = "${module.demo-node.demo_node_instance_id}" }
+output "demo_node_ip"    { value = "${module.demo-node.demo_node_ip}" }
+output "demo_node_sg_id" { value = "${module.demo-node.demo_node_sg_id}" }
+
+output "demo_node_dependency_id"    { value = "${module.demo-node.demo_node_instance_id}" }
+output "demo_node_dependency_ip"    { value = "${module.demo-node.demo_node_ip}" }
+output "demo_node_dependency_sg_id" { value = "${module.demo-node.demo_node_dependency_sg_id}" }

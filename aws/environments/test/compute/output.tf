a70b22433c1fdb6b3592cf33984484b31f84d054
jordzn <jordan.taylor@sendachi.com>
2016-08-20 15:11:28 +0100

    Amended demo-node dependency outputs

--- aws/environments/test/compute/output.tf
+++ aws/environments/test/compute/output.tf
@@ -4,6 +4,6 @@ output "demo_node_id"    { value = "${module.demo-node.demo_node_instance_id}" }
 output "demo_node_ip"    { value = "${module.demo-node.demo_node_ip}" }
 output "demo_node_sg_id" { value = "${module.demo-node.demo_node_sg_id}" }
 
-output "demo_node_dependency_id"    { value = "${module.demo-node.demo_node_instance_id}" }
-output "demo_node_dependency_ip"    { value = "${module.demo-node.demo_node_ip}" }
-output "demo_node_dependency_sg_id" { value = "${module.demo-node.demo_node_dependency_sg_id}" }
+output "demo_node_dependency_id"    { value = "${module.demo-node-dependency.demo_node_instance_id}" }
+output "demo_node_dependency_ip"    { value = "${module.demo-node-dependency.demo_node_ip}" }
+output "demo_node_dependency_sg_id" { value = "${module.demo-node-dependency.demo_node_dependency_sg_id}" }

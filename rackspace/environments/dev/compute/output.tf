e5346db6c5023e6a21987d8ced582d7544c4ff90
jordzn <jordan.taylor@sendachi.com>
2016-08-20 15:03:05 +0100

    Removal of RS

--- rackspace/environments/dev/compute/output.tf
+++
@@ -1,5 +0,0 @@
-# outputs produced at the end of a terraform apply: id of the instances, id of
-# security group(s) they are part of
-output "demo_node_id"    { value = "${module.demo-node.demo_node_instance_id}" }
-output "demo_node_ip"    { value = "${module.demo-node.demo_node_ip}" }
-output "demo_node_sg_id" { value = "${module.demo-node.demo_node_sg_id}" }
--- rackspace/environments/dev/compute/terraform.tfvars
+++
@@ -1,8 +0,0 @@
-image_id      = ""
-#contact       = ""
-
-# Demo Node #
-demo_node_flavor_id      = "3"
-demo_node_instance_count = "1"
-#demo_node_ssh_key_pair   = ""
-#demo_node_admin_pass     = ""

5658018bb6c6e48fcc7d70de105fa6e680690b0c
jordzn <jordan.taylor@sendachi.com>
2016-08-21 10:57:45 +0100

    Added all Azure environments

---
+++ azure/environments/test/compute/output.tf
@@ -0,0 +1,4 @@
+# outputs produced at the end of a terraform apply: id and ip of the instances, id of
+# security group(s) they are part of
+output "demo_node_id"    { value = "${module.demo-node.demo_node_instance_id}" }
+output "demo_node_ip"    { value = "${module.demo-node.demo_node_ip}" }

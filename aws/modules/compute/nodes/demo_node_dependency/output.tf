ad4652c2ff848dc0ff1a97fe3c0b314ee0da44c5
Jordan Taylor <jordan.taylor@contino.io>
2016-10-28 17:55:25 +0100

    adding dummy value for output

--- aws/modules/compute/nodes/demo_node_dependency/output.tf
+++ aws/modules/compute/nodes/demo_node_dependency/output.tf
@@ -3,3 +3,4 @@
 output "demo_node_dependency_instance_id" { value = [ "${aws_instance.demo-node-dependency.*.id}" ] }
 output "demo_node_dependency_ip"          { value = [ "${aws_instance.demo-node-dependency.*.public_ip}" ] }
 output "demo_node_dependency_sg_id"       { value = "${aws_security_group.demo-dependency-sg.id}" }
+output "demo_node_dependency_value"       { value = "dummy_value" }
